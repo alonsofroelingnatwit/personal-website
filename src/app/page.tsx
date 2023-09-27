58fee23d03964110ef6e3af7f273d02c7d993f28
Nicolas Alonso Froeling <nicolasalonsofroeling@Nicolass-MacBook-Pro.local>
2023-09-27 15:01:01 -0400

    analytics for vercel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { delay, motion } from 'framer-motion';
+import { motion } from 'framer-motion';
 import Image from 'next/image'
 import Link from 'next/link';
 import { ReactNode, useEffect, useState } from 'react';
